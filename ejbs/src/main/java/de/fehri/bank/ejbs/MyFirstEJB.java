d4acbc8d8fe21c94529e5d37308cb44f11c47dea
fehri <philip.fehrenbacher@gmx.de>
2017-08-08 22:32:08 +0200

    Update EJB, create Servlet

--- ejbs/src/main/java/de/fehri/bank/ejbs/MyFirstEJB.java
+++ ejbs/src/main/java/de/fehri/bank/ejbs/MyFirstEJB.java
@@ -2,8 +2,18 @@ package de.fehri.bank.ejbs;
 
 
 import javax.ejb.Stateless;
+import java.util.Random;
 
 @Stateless
 public class MyFirstEJB {
+
+
+    public int myLuckyNumber() {
+
+        Random randomGenerator = new Random();
+
+        return randomGenerator.nextInt(100);
+
+    }
 
 }
